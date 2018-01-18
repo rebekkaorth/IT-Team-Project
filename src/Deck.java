d4b0c99de40c0245ecc01c10b04eb29cad5cb28d
Neil Kennedy <2025066K@student.gla.ac.uk>
2018-01-18 16:15:43 +0000

    Updated Deck class for attribute names

--- src/Deck.java
+++ src/Deck.java
@@ -6,6 +6,7 @@ public class Deck {
 
 	private Card[] deckArray;
 	private static final int deckSize = 40;
+	private String att1Name,att2Name,att3Name,att4Name,att5Name;
 	
 	/*
 	 * Constructor. Creates card object and adds to the deck
@@ -16,7 +17,8 @@ public class Deck {
 		
 		try {
 			Scanner scan = new Scanner(new File(textFile));
-			String headings = scan.nextLine(); //Not currently used	
+			String headings = scan.nextLine(); //Not currently used
+			setCategoryNames(headings);
 			while (scan.hasNextLine()) {
 				String line = scan.nextLine(); //Line to build card object
 				Card newCard = new Card(line);
@@ -47,6 +49,43 @@ public class Deck {
 		}
 	}
 	
+	
+	/*
+	 * Set the category names
+	 */
+	public void setCategoryNames(String lineIn) {
+		String categoryArray[] = lineIn.split(" ");
+		att1Name = categoryArray[0];
+		att2Name = categoryArray[1];
+		att3Name = categoryArray[2];
+		att4Name = categoryArray[3];
+		att5Name = categoryArray[4];
+		
+	}
+	
+	
+	
+	/*
+	 * Getters for category names
+	 */
+	
+	public String getAtt1Name() {
+		return att1Name;
+	}
+	public String getAtt2Name() {
+		return att2Name;
+	}
+	public String getAtt3Name() {
+		return att3Name;
+	}
+	public String getAtt4Name() {
+		return att4Name;
+	}
+	public String getAtt5Name() {
+		return att5Name;
+	}
+	
+	
 	/*
 	 * Main method for testing
 	 */
@@ -56,7 +95,8 @@ public class Deck {
 			for(int i=0; i<deckSize; i++) {
 			testDeck.shuffleDeck();
 			Card c = testDeck.deckArray[i];
-		System.out.println(c.getDescription()+" "+c.getAtt1()+" "+c.getAtt2()+" "+c.getAtt3()+" "+c.getAtt4()+" "+c.getAtt5()+" "+c.getid());
+		//	System.out.println(c.getDescription()+" "+c.getAtt1()+" "+c.getAtt2()+" "+c.getAtt3()+" "+c.getAtt4()+" "+c.getAtt5()+" "+c.getid());
+		//	System.out.println(testDeck.getAtt1Name()+" "+testDeck.getAtt2Name()+" "+testDeck.getAtt3Name()+" "+testDeck.getAtt4Name()+" "+testDeck.getAtt5Name());
 		//System.out.println(c.getid());
 			}
 	}
