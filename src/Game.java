815647d37ff99c3969ff0186a540fd6011e7dd63
Shan <ggf554@hotmail.com>
2018-01-19 13:06:48 +0000

    game.java: working at main game methord

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,41 @@
 public class Game {
-	
-public String[] players;
-public String[] allPlayers;
 
-public int roundCount;
-public int numberOfDraws;
-	private int playernumber =5;
-	
-	
+	public Player[] players;
+	public Player[] allPlayers;
+
+	public int roundCount;
+	public int numOfDraws;
+
+	private int playernumber = 5;
+
+	Deck deck;
+
 	public Game() {
 		roundCount = 0;
-		numberOfDraws =0;
+		numOfDraws = 0;
+		
+
+	}
+
+	
+	
+	public void game() {
+		deck  = new Deck();
 		setUpPlayers();
 		
 		
 	}
 	
+	
+	
 	/**
 	 * set up 1 human and 4 AI player
 	 */
-	public void setUpPlayers(){
-		
-		players = new String[playernumber];
-		for(int i=0; i<playernumber; i++) {
-			if(i==0) {
+	public void setUpPlayers() {
+
+		players = new Player[playernumber];
+		for (int i = 0; i < playernumber; i++) {
+			if (i == 0) {
 				players[i] = "human";
 				allPlayers[i] = "human";
 			} else {
@@ -32,65 +44,128 @@ public int numberOfDraws;
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * it will return the winner of the turn (from player1 - 5)
-	 * @param player1value value of the card from player1
-	 * @param player2value value of the card from player1
-	 * @param player3value value of the card from player1
-	 * @param player4value value of the card from player1
-	 * @param player5value value of the card from player1
+	 * 
+	 * @param player1value
+	 *            value of the card from player1
+	 * @param player2value
+	 *            value of the card from player1
+	 * @param player3value
+	 *            value of the card from player1
+	 * @param player4value
+	 *            value of the card from player1
+	 * @param player5value
+	 *            value of the card from player1
 	 * @return the winner of the round, draw if the return value is 0
 	 */
-	public int roundWinner(int player1value,int player2value,int player3value,int player4value,int player5value) {
-		int winner =0;
+	public int round(int player1value, int player2value, int player3value, int player4value, int player5value) {
+		int winner = 0;
 		int max = 0;
-		int[] value = {player1value, player2value, player3value, player4value, player5value};
-		for(int i=0; i<5; i++) {
-			if(max == 0) {
+		int[] value = { player1value, player2value, player3value, player4value, player5value };
+		for (int i = 0; i < 5; i++) {
+			if (max == 0) {
 				max = value[i];
-				winner = i+1;
-			}else {
-				if(value[i]>max) {
+				winner = i + 1;
+			} else {
+				if (value[i] > max) {
 					max = value[i];
-					winner = i+1;
+					winner = i + 1;
 				}
 			}
 		}
-		
+
 		return winner;
 	}
-	
+
 	/**
 	 * div the deck in to 5 part, save it in to player personalDeck
 	 */
-	public dealCards() {
-		
-		
-		
+	public void dealCards() {
+
+		for (int i = 0; i < 5; i++) {
+
+			for (int ia = 1; ia < 9; ia++) {
+
+				player[i].setPersonalDeck = deck.deckArry[(i * 8 + ia)];
+
+			}
+		}
+
 	}
-	
+
+	/**
+	 * select a random player as the starting player
+	 * 
+	 * @return one random player from 5 players
+	 */
+	public int selectStartingPlayer() {
+
+		int playerNumber = (int) (Math.random() * 5);
+
+		return playerNumber;
+	}
+
+	/**
+	 * 
+	 * @param player
+	 *            which player
+	 * @return the card left in the deck for the player
+	 */
+	public int getCardsLeftInDeck(Player player) {
+		int cardLeft = 0;
+
+		player.getNumOfCardsInDeck();
+
+		return cardLeft;
+	}
+
+	//??
+	public void chooseCategory() {
+
+	}
+
+	/**
+	 * 
+	 * @param player which player
+	 * @param category the index of category choosen
+	 * @return the value the category of the first card in this player 
+	 */
+	public int getCategoryValueOfPlayer(Player player, int category) {
+		int value = 0;
+
+		value = player.getFirstCard.getAtt(category); // card class need change
+
+		return value;
+	}
+
 	/**
 	 * 
 	 */
 	public void upDatePlayers() {
 		int lostCount = 0;
-		for(int i=0; i<playernumber; i++) {
-			if(players[i].card==0) {
-			
+		for (int i = 0; i < playernumber; i++) {
+			if (players[i].card == 0) {
+
 				players[i] = "empty";
 				lostCount++;
 			}
 		}
-		
-		if(lostCount ==( playernumber -1)) {
+
+		if (lostCount == (playernumber - 1)) {
 			System.out.println("Game finish.");
 		}
-		
-		
+
 	}
-	
-	
+
+	/**
+	 * 
+	 * @return the number of draws
+	 */
+	public int getNumOfDraws() {
+
+		return numOfDraws;
+	}
 
 }
