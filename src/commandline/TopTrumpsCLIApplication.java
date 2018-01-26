f2bd6dcb93d93331d65048c1e5525ea2d9b42731
Neil Kennedy <2025066K@student.gla.ac.uk>
2018-01-26 14:40:50 +0000

    Updated CLIApplication class

--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -1,4 +1,6 @@
 package commandline;
+
+import java.util.*;
 /**
  * Top Trumps command line application
  */
@@ -11,22 +13,40 @@ public class TopTrumpsCLIApplication {
 	 */
 	public static void main(String[] args) {
 
+
 		boolean writeGameLogsToFile = false; // Should we write game logs to file?
-		if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
-		
-		// State
 		boolean userWantsToQuit = false; // flag to check whether the user wants to quit the application
-		
+
+		if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
+
 		// Loop until the user wants to exit the game
 		while (!userWantsToQuit) {
+			Scanner scan = new Scanner(System.in);
+			String input = scan.next().toLowerCase();
+			boolean userInput = false;
+			while(!userInput){
+				System.out.println("Do you want to play a game or see statistics?");
+				if(input.equals("play") || input.equals("statistics") || input.equals("quit")){
+					userInput = true;
+				}
+				else{
+					System.out.println("Input not recognised, please enter either: play, statistics, or quit");
+				}
 
-			// ----------------------------------------------------
-			// Add your game logic here based on the requirements
-			// ----------------------------------------------------
-//			//Neil inserts here
-
+			}
+			if(input.equals("play")){
+				Game game = new Game(5,writeGameLogsToFile);
+				game.playGame();
+			}
+			else if(input.equals("statistics")){
+				Game game = new Game(5, writeGameLogsToFile);
+			// Get info from DB	game.showData();
+			}
+			else if(input.equals("quit")){
+				userWantsToQuit = true;
+			}
 		}
-
+		System.exit(0);
 	}
 
 }
